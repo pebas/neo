6cd22c44a8007037a551b797818192b4fcb8d034
Predrag Stojanović <3474337+pebas@users.noreply.github.com>
2018-12-31 17:29:39 +0100

    Revert "Revert "Test Another""
    This reverts commit ae9fec53783041ecf617cb3c18acd195d512b132.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,46 @@
+// Define JS
+'use strict';
+
+// Call Gulp
 var gulp = require('gulp');
-var sass = require('sass');
-gulp.task('regards', function () {
-  console.log('Hello World!');
+
+// CSS Preprocessors
+var sass = require('gulp-sass');
+
+// Live Reload
+var browserSync = require('browser-sync').create();
+
+// useref plugin
+var useref = require('gulp-useref');
+
+// Define BrowserSync Task
+gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: 'app'
+      },
+    });
+  });
+
+  // USerref
+  gulp.task('useref', function(){
+    return gulp.src('app/*.html')
+      .pipe(useref())
+      .pipe(gulp.dest('dist'))
+  });
+// Styles
+gulp.task('styles', function () {
+    return gulp.src('dist/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
+// Watch
+gulp.task('watch', gulp.parallel( 'browserSync', 'styles', function (done) {
+    gulp.watch('dist/scss/**/*.scss', gulp.series('styles'));
+    done();
+}));
