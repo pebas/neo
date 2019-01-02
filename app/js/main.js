86b9ddd8f8df73660c7892b0fa3c1c40cab0c967
Predrag Stojanović <3474337+pebas@users.noreply.github.com>
2019-01-02 12:53:01 +0100

    2.2018.

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,49 @@
-var a = 3;
-console.log(a)
-a -= 12;
-console.log(a)
+var ime = ["Slobodno!", "Dvojka!", "Trojka!", "A kako promasujes!!!"]
 
+function kosarkaskiPoeni(poen) {
+    var answer = ime;
+    switch (poen) {
+        case 1: 1;
+            answer = ime[0];
+            break;
+        case 2: 2;
+            answer = ime[1];
+            break;
+        case 3: 3;
+            answer = ime[2];
+            break;
+        default:
+            answer = ime[3];
+            break;
+    }
+    return answer;
 
+}
+
+console.log(kosarkaskiPoeni(2));
+
+/*
+function caseInSwitch(val) {
+    var answer = "";
+    switch (val) {
+        case 1: "Bob";
+            answer = "Marley";
+            break;
+        case 2:
+            answer = "Beta";
+            break;
+        case 3:
+            answer = "Gamma";
+            break;
+        case 4:
+            answer = "Delta";
+            break;
+        default:
+            answer = "Nothing selected!";
+            break;
+    }
+    return answer;
+}
+
+console.log(caseInSwitch("Bob"));
+*/
