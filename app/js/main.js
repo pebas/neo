cce537f8898b9f3a5d73241e3b51451c315de5c4
Predrag Stojanović <3474337+pebas@users.noreply.github.com>
2019-01-03 10:38:58 +0100

    3.2.2018

--- app/js/main.js
+++ app/js/main.js
@@ -1,46 +1,15 @@
-var imenaZaPoene = ["Slobodno!", "Dvojka!", "Trojka!", "A kako promasujes!!!"]
-
-function kosarkaskiPoeni(poen) {
-    var vratiIme = imenaZaPoene;
-    switch (poen) {
-        case 1: 1;
-            vratiIme = imenaZaPoene[0];
-            break;
-        case 2: 2;
-            vratiIme = imenaZaPoene[1];
-            break;
-        case 3: 3;
-            vratiIme = imenaZaPoene[2];
-            break;
-        default:
-            vratiIme = imenaZaPoene[3];
-            break;
-    }
-    return vratiIme;
+let myObj = {
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
 }
 
-console.log(kosarkaskiPoeni(1));
-
-function caseInSwitch(val) {
-    var answer = "";
-    switch (val) {
-        case 1: "Bob";
-            answer = "Marley";
-            break;
-        case 2:
-            answer = "Beta";
-            break;
-        case 3:
-            answer = "Gamma";
-            break;
-        case 4:
-            answer = "Delta";
-            break;
-        default:
-            answer = "Nothing selected!";
-            break;
+function checkObj(checkProp) {
+    if (myObj.hasOwnProperty(checkProp)) {
+        return myObj[checkProp];
+    } else {
+        return "Your object have no such property."
     }
-    return answer;
 }
 
-console.log(caseInSwitch("Bob"));
+console.log(checkObj("gift"));
