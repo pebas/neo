4a16672f4bb6eb544c7c3c44b090f073149cc9ee
Predrag Stojanović <3474337+pebas@users.noreply.github.com>
2019-01-09 17:14:50 +0100

    9.1..2019
    8kyu

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,23 @@
-let myObj = {
-    gift: "pony",
-    pet: "kitten",
-    bed: "sleigh"
-}
+function countPositivesSumNegatives(input) {
+    let countPositive = 0;
+    let sumNegative = 0;
+    if(input && input.length > 0) {
+        for (let i = 0; i < input.length; i++) {
+            if (input[i] > 0) {
+                countPositive++;
+            } else if (input[i] < 0) {
+                sumNegative += input[i];
+            }
 
-function checkObj(checkProp) {
-    if (myObj.hasOwnProperty(checkProp)) {
-        return myObj[checkProp];
-    } else {
-        return "Your object have no such property."
+        }
+        return [countPositive, sumNegative];
     }
+    return [];
+
 }
 
-console.log(checkObj("gift"));
+
+
+console.log(countPositivesSumNegatives([]));
+
+
